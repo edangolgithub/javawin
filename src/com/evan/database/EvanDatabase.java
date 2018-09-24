39d3ddd084f45a8b78537ccb3e7212d9d16b9e7a
evan dangol <dangolevan@gmail.com>
2018-09-24 18:22:37 +0545

    secondcommit

--- src/com/evan/database/EvanDatabase.java
+++ src/com/evan/database/EvanDatabase.java
@@ -1,9 +1,43 @@
 package com.evan.database;
 
+import java.sql.*;
+
+
 public class EvanDatabase {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc."
+					+ "Driver");
+			String url="jdbc:mysql://localhost/EMP?useSSL=false";
+			Connection Con=DriverManager.getConnection(url,"root","mysql");
+			String sql="select * from Employees";
+			PreparedStatement st=Con.prepareStatement(sql);
+			
+			ResultSet rs=st.executeQuery();
+			
+			while(rs.next())
+			{
+				int id  = rs.getInt("id");
+			       int age = rs.getInt("age");
+			       String first = rs.getString("first");
+			       String last = rs.getString("last");
+
+			       //Display values
+			       System.out.print("ID: " + id);
+			       System.out.print(", Age: " + age);
+			       System.out.print(", First: " + first);
+			       System.out.println(", Last: " + last);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
